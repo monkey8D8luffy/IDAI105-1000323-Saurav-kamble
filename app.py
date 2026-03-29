1aecd60b87a60991e8799fbeb5ee3bfd84be8166
monkey8D8luffy <saurav.kamble2008@gmail.com>
2026-03-29 18:53:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.title("🛍️ Beyond Discounts: Black Friday Sales Insights")
 def load_data():
     # Using the smaller sample dataset to bypass GitHub's 25MB limit
     # If you used the ZIP method instead, change this to 'BlackFriday_Cleaned.zip'
-    df = pd.read_csv('BlackFriday_Sample.csv')
+    pd.read_csv('BlackFriday_Cleaned.zip')
     return df
 
 df = load_data()
